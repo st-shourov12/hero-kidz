09b38b20e3fc317a925896d4ecdc352fa4497f65
st-shourov12 <shourovsc38@gmail.com>
2026-04-13 22:05:37 +0600

    adding mongodb

---
+++ src/app/layout.jsx
@@ -0,0 +1,35 @@
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
+
+const poppins = Poppins({
+  weight:["100","200","400","500","600","700","800"]
+})
+export const metadata = {
+  title: "Hero-Kidz",
+  description: "Using  next app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      
+    >
+      <body className={`${poppins.className}antialiased`}>
+
+        <header className="py-2 md:max-w-11/12 mx-auto">
+          <Navbar></Navbar>
+        </header>
+        <main className="py-2 md:max-w-11/12 mx-auto">
+
+          {children}
+        </main>
+        <footer>
+          <Footer></Footer>
+        </footer>
+      </body>
+    </html>
+  );
+}
