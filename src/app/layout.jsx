3977769e1af93c2cde92ecd780e65383b7ebcc4a
st-shourov12 <shourovsc38@gmail.com>
2026-04-13 22:26:42 +0600

    creating logo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layouts/Navbar";
 import Footer from "@/components/layouts/Footer";
@@ -17,13 +17,12 @@ export default function RootLayout({ children }) {
       lang="en"
       
     >
-      <body className={`${poppins.className}antialiased`}>
+      <body className={`${poppins.className} antialiased`}>
 
         <header className="py-2 md:max-w-11/12 mx-auto">
           <Navbar></Navbar>
         </header>
         <main className="py-2 md:max-w-11/12 mx-auto">
-
           {children}
         </main>
         <footer>
